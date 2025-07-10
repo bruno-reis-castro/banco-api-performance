3edc9803c814526cf81b42ff11d53ee82115d798
bruno-reis-castro <bruno.reis.castro1@gmail.com>
2025-07-09 22:00:05 -0300

    Adicionando stages - performance

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,8 +4,14 @@ import {sleep, check} from 'k6'
 //set K6_WEB_DASHBOARD=true K6_WEB_DASHBOARD_EXPORT=html-report.html k6 run script.js
 export const options = {
     //iterations: 15,
-    vus: 10,
-    duration: '30s',
+    stages: [
+        { duration: '10s', target: 10},
+        { duration: '20s', target: 10},
+        { duration: '10s', target: 30},
+        { duration: '20s', target: 30},
+        { duration: '20s', target: 0},
+        
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
         http_req_failed: ['rate<0.01']
